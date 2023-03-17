215700593c208f5078185c2783a271c63c796024
adarsh-2425 <you@example.comadarsh.lol2425@gmail.com>
2023-03-17 11:19:13 +0530

    lol

--- events.js
+++ events.js
@@ -1,3 +1,7 @@
+const express = require('express');
+
+const app = express();
+
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
@@ -8,3 +12,15 @@ emitter.on('messageLogged', e =>{
 
 //Register an event
 emitter.emit('messageLogged');
+
+app.get('/', (req,res)=>{
+    res.send('Invalid endpoint')
+});
+
+app.get('/lol',(req,res)=>{
+    res.send('lol happens');
+})
+
+app.listen(3000, e=>{
+    console.log('server running on port 3000');
+})
