4337b50db5ed3b5cc002fbf81771224c2d2f81ce
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-02 10:32:56 +0530

    Create products.mjs

---
+++ Modules/Products/products.mjs
@@ -0,0 +1,19 @@
+// products.js
+
+export const products = [
+  {
+    id: 1,
+    name: 'Product A',
+    price: 10.99,
+    description: 'Description for Product A'
+  },
+  {
+    id: 2,
+    name: 'Product B',
+    price: 19.99,
+    description: 'Description for Product B'
+  },
+];
+
+
+
