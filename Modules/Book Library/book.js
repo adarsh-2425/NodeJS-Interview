5b8eeda64cce4060540a93c69a6f5f84ead6f5e6
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-09-02 11:56:56 +0530

    Create book.js

---
+++ Modules/Book Library/book.js
@@ -0,0 +1,10 @@
+// Book Constructor
+
+function Book(title, author, isbn, pages) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+  this.pages = pages;
+}
+
+module.exports = Book;
